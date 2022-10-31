3015d1b876a1c7f0526fb5d0bfa21075164a5679
Vaibhav_Sharma2 <vaibhavshrma@hotmail.com>
2022-10-31 17:09:10 +0530

    Adding test cases for promotion engine. Applies promo code rules on cart items and verifies result.

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,24 +3,27 @@ package org.example;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.promotion.PromotionApplicator;
 
-/**
+/*
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
 {
-    /**
+    /*
      * Create the test case
      *
      * @param testName name of the test case
      */
+    PromotionApplicator promoApplicator = null;
     public AppTest( String testName )
     {
         super( testName );
+        promoApplicator = new PromotionApplicator();
     }
 
-    /**
+    /*
      * @return the suite of tests being tested
      */
     public static Test suite()
@@ -28,12 +31,22 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
 
-        assertTrue( true );
+    public void testCase1() {
+        char[] cartItems = new char[]{'A','B','C'};
+        int cartPrice = promoApplicator.applyPromotions(cartItems);
+        assertTrue( cartPrice == 100 );
+    }
+
+    public void testCase2() {
+        char[] cartItems = new char[]{'A','A','A','A','A','B','B','B','B','B','C'};
+        int cartPrice = promoApplicator.applyPromotions(cartItems);
+        assertTrue( cartPrice == 370 );
+    }
+
+    public void testCase3() {
+        char[] cartItems = new char[]{'A','A','A','B','B','B','B','B','C','D'};
+        int cartPrice = promoApplicator.applyPromotions(cartItems);
+        assertTrue(cartPrice == 280);
     }
 }
